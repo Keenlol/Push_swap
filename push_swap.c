a59dac69eaffad562fc8f1017cf921f63d438f09
Chetphisuth Tongpa <chetphisuth.t@ku.th>
2025-01-10 14:55:54 +0700

    initial commit

---
+++ push_swap.c
@@ -0,0 +1,17 @@
+#include "push_swap.h"
+
+void	predeict(t_stack *stack_a, t_stack *stack_b, ssize_t i)
+{
+	
+}
+
+void	push_swap(t_stack *stack_a, t_stack *stack_b)
+{
+	use("pb", stack_a, stack_b);
+	use("pb", stack_a, stack_b);
+
+
+
+}
+
+
