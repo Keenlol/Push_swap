1428a56c66cd1ea8e82798e223535c440af0216a
Chetphisuth Tongpa <chetphisuth.t@ku.th>
2025-01-15 17:08:56 +0700

    rotate the stack before push

--- push_swap.c
+++ push_swap.c
@@ -12,29 +12,11 @@ int		valid(t_stack *stack_b, ssize_t i, int num)
 	else
 		num_above = stack_b->arr[i+1];
 	num_below = stack_b->arr[i];
-
-	printf("%d <= %d <= %d\n", num_below, num, num_above);
-
 	if ((num_below <= num && num <= num_above) ||
 		(num_below == stack_b->greatest && 
 		(num <= num_above || stack_b->greatest <= num)))
 		return (1);
 	return (0);
-	// if (i == i_b_top)
-	// {
-	// 	if (stack_b->arr[i_b_top] <= num && num <= stack_b->arr[0])
-	// 		return (1);
-	// 	if (num >= stack_b->greatest && stack_b->arr[i_b_top] == stack_b->greatest)
-	// 		return (1);
-	// 	return (0);
-	// }
-	// else
-	// {
-	// 	if (stack_b->arr[i] <= num && num <= stack_b->arr[i+1])
-	// 		return (1);
-	// 	return (0);
-	// }
-	// return (0);
 }
 
 void	rotate(t_stack *stack_a, t_stack *stack_b, int a_num_top)
@@ -46,8 +28,17 @@ void	rotate(t_stack *stack_a, t_stack *stack_b, int a_num_top)
 	while (!valid(stack_b, i, stack_a->arr[stack_a->i_top]))
 		i--;
 	printf("\ni = %ld\n", i);
-
-
+	if (i >= stack_b->i_top / 2)
+	{
+		i = stack_b->i_top - i;
+		while (i-- > 0)
+			use("rb", stack_a, stack_b);
+	}
+	else
+	{
+		while (i-- > 0)
+			use("rrb", stack_a, stack_b);
+	}
 }
 
 void	push_swap(t_stack *stack_a, t_stack *stack_b)
