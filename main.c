e398ad250355be64c293498d1cb3046eb29ce965
Chetphisuth Tongpa <chetphisuth.t@ku.th>
2025-01-20 14:00:13 +0700

    remove printing stacks

--- main.c
+++ main.c
@@ -9,11 +9,12 @@ int	main(int argc, char *argv[])
 		error_exit();
 	create_stack_a(&stack_a, argv, argc - 1);
 	if (dup_error(&stack_a))
+	{
+		free(stack_a.arr);
 		error_exit();
+	}
 	create_stack_b(&stack_b, argc - 1);
-	print_stacks(&stack_a, &stack_b);
 	push_swap(&stack_a, &stack_b);
-	print_stacks(&stack_a, &stack_b);
 	free(stack_a.arr);
 	free(stack_b.arr);
 }
